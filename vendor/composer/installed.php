c58e19532c8085dd01dd2bb2dbb4a9b32bfcba62
Indo Shop <support@indoshop.site>
2024-11-28 16:24:27 -0500

    make it better

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'avonixor/check-mail-web',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f343ebd72cf1a6386c10828894356ec6176b916',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'avonixor/check-mail-web' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f343ebd72cf1a6386c10828894356ec6176b916',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
